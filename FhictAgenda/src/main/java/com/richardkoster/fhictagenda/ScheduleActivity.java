d17e3d17dc54662f4a21e0efd3a3eef564df89c8
Marijn van der Werf <marijn.vanderwerf@gmail.com>
2013-08-08 16:35:09 +0200

    Add ViewServer to aid in debugging

--- FhictAgenda/src/main/java/com/richardkoster/fhictagenda/ScheduleActivity.java
+++ FhictAgenda/src/main/java/com/richardkoster/fhictagenda/ScheduleActivity.java
@@ -3,6 +3,8 @@ package com.richardkoster.fhictagenda;
 import android.app.Activity;
 import android.os.Bundle;
 
+import com.android.debug.hv.ViewServer;
+
 public class ScheduleActivity extends Activity {
 
     @Override
@@ -10,5 +12,19 @@ public class ScheduleActivity extends Activity {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_schedule);
+
+        ViewServer.get(this).addWindow(this);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        ViewServer.get(this).removeWindow(this);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        ViewServer.get(this).setFocusedWindow(this);
     }
 }
